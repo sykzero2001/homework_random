6a62d344e7ddd23e84dfdfb37a6b27aa4aa5ca62
Dante <deathzeroking@hotmail.com>
2015-10-06 20:08:32 +0800

    新增

--- homework_random/ViewController.m
+++ homework_random/ViewController.m
@@ -17,11 +17,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSMutableArray *sortedArray = [self getrandomarray] ;
+    NSNumber *S_number = sortedArray[0];
+    NSNumber *F_number = sortedArray[1];
+    while(S_number.intValue==F_number.intValue)
+    {
+        sortedArray = [self getrandomarray];
+        S_number = sortedArray[0];
+        F_number = sortedArray[1];
+        
+    }
+    
+    int sum = 0;
+    for(int i = S_number.intValue ;i<=F_number.intValue;i++)
+    {
+        if (i % 2 != 0) {
+            sum = sum + i;
+        }
+        else
+        {
+            sum = sum;
+        }
+    }
+    NSLog(@"從%@到%@,總和為%d",S_number,F_number,sum);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(NSArray*)getrandomarray
+{
+    NSMutableArray *array;
+    array = [NSMutableArray new];
+    for (int i = 1; i <= 2 ; i++) {
+        
+        int n = arc4random() % 1000;
+        [array addObject:[NSNumber numberWithInt:n]];
+    };
+    NSArray *sortedArray= [array sortedArrayUsingSelector:@selector(compare:)];
+    return sortedArray;
+    
 }
 
 @end
